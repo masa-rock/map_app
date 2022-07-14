99b0e276161e75666ffb091d26f1234782f8fb2c
masa-rock <79382412+masa-rock@users.noreply.github.com>
2022-07-14 22:30:55 +0900

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'maps/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: "maps#index"
+end
