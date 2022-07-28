ba4d8e789c66e30c43af89248271e59d792557f3
masa-rock <79382412+masa-rock@users.noreply.github.com>
2022-07-28 23:40:10 +0900

    スポット登録ページの作成

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,25 @@
 class PagesController < ApplicationController
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to pages_path, notice: 'スポットが追加されました。'
+    else
+      render :new
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
   def index
   end
 
   def show
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :address, :business_hours_start, :business_hours_end, :fee, :eat_walk, :stay_time, :description, images: [])
   end
 end
